a3bf5e0ba9295e93322c1b9276537b9dd194f025
FateZeros <fatejane@163.com>
2024-03-26 10:11:41 +0800

    update

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,2 +1,24 @@
 module.exports = {
+    root: true,
+	env: {
+		browser: true,
+		es2021: true,
+		node: true,
+	},
+	parser: 'vue-eslint-parser',
+	extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:vue/vue3-essential'],
+	parserOptions: {
+		ecmaVersion: 'latest',
+		parser: '@typescript-eslint/parser',
+		sourceType: 'module',
+	},
+	plugins: ['@typescript-eslint', 'vue'],
+	rules: {
+		// 要求组件名称为 multi word
+		'vue/multi-word-component-names': 'off',
+		// 禁止使用 v-html 以预防 XSS 攻击
+		'vue/no-v-html': 'off',
+		// 禁止使用后缀运算符 ! 的非空断言
+		'@typescript-eslint/no-non-null-assertion': 'off',
+	},
 };
